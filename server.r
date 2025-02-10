a2a873787d2b90fb17c4bc2f6f9e54099d6c253b
sharp-lilac <161522137+sharp-lilac@users.noreply.github.com>
2025-02-10 14:39:06 -0600

    Expand report Rmd options

--- server.r
+++ server.r
@@ -3,93 +3,93 @@
 # Define server ---------------------------
 server <- function(input, output, session) {
     # Define dataframes from uploads
-    df_upload_catch_1per <- reactive({
-        req(input$upload_catch_1per)
-        read.csv(input$upload_catch_1per$datapath)
+    df_upload_fisheries_1per <- reactive({
+        req(input$upload_fisheries_1per)
+        read.csv(input$upload_fisheries_1per$datapath)
     })
-    df_upload_catchproj_1per <- reactive({
-        req(input$upload_catchproj_1per)
-        read.csv(input$upload_catchproj_1per$datapath)
+    df_upload_fisher_1per <- reactive({
+        req(input$upload_fisher_1per)
+        read.csv(input$upload_fisher_1per$datapath)
     })
-    df_upload_lamp_1per <- reactive({
-        req(input$upload_lamp_1per)
-        read.csv(input$upload_lamp_1per$datapath)
+    df_upload_lampgen_1per <- reactive({
+        req(input$upload_lampgen_1per)
+        read.csv(input$upload_lampgen_1per$datapath)
     })
-    df_upload_spag_1per <- reactive({
-        req(input$upload_spag_1per)
-        read.csv(input$upload_spag_1per$datapath)
+    df_upload_spagvis_1per <- reactive({
+        req(input$upload_spagvis_1per)
+        read.csv(input$upload_spagvis_1per$datapath)
     })
 
     # Validate dataframes
-    observeEvent(input$validate_catch_1per, {
+    observeEvent(input$validate_fisheries_1per, {
         shinyalert("Success!", "Validation Successful!")
     })
-    observeEvent(input$validate_catchproj_1per, {
+    observeEvent(input$validate_fisher_1per, {
         shinyalert("Success!", "Validation Successful!")
     })
-    observeEvent(input$validate_lamp_1per, {
+    observeEvent(input$validate_lampgen_1per, {
         shinyalert("Success!", "Validation Successful!")
     })
-    observeEvent(input$validate_spag_1per, {
+    observeEvent(input$validate_spagvis_1per, {
         shinyalert("Success!", "Validation Successful!")
     })
 
     # Create single period (1per) reports
-    output$report_catch_1per <- downloadHandler(
-        filename = "report_catch_1per.docx",
+    output$report_fisheries_1per <- downloadHandler(
+        filename = "report_fisheries_1per.docx",
         content = function(file) {
-            src <- normalizePath(c("reports/report_catch_1per.Rmd", "reports/report_template.docx", "www/images/TASA_logo_full_color.png"))
+            src <- normalizePath(c("reports/report_fisheries_1per.Rmd", "reports/report_template.docx", "www/images/TASA_logo_full_color.png"))
             owd <- setwd(tempdir())
             on.exit(setwd(owd))
-            file.copy(src, c("report_catch_1per.Rmd", "report_template.docx", "TASA_logo_full_color.png"), overwrite = TRUE)
+            file.copy(src, c("report_fisheries_1per.Rmd", "report_template.docx", "TASA_logo_full_color.png"), overwrite = TRUE)
             out <- render(
-                "report_catch_1per.Rmd",
-                params = list(user_name = input$name, datafile = df_upload_catch_1per()),
+                "report_fisheries_1per.Rmd",
+                params = list(user_name = input$name, datafile = df_upload_fisheries_1per()),
                 envir = new.env(parent = globalenv())
             )
             file.rename(out, file)
         }
     )
-    output$report_catchproj_1per <- downloadHandler(
-        filename = "report_catchproj_1per.docx",
+    output$report_fisher_1per <- downloadHandler(
+        filename = "report_fisher_1per.docx",
         content = function(file) {
-            src <- normalizePath(c("reports/report_catchproj_1per.Rmd", "reports/report_template.docx", "www/images/TASA_logo_full_color.png"))
+            src <- normalizePath(c("reports/report_fisher_1per.Rmd", "reports/report_template.docx", "www/images/TASA_logo_full_color.png"))
             owd <- setwd(tempdir())
             on.exit(setwd(owd))
-            file.copy(src, c("report_catchproj_1per.Rmd", "report_template.docx", "TASA_logo_full_color.png"), overwrite = TRUE)
+            file.copy(src, c("report_fisher_1per.Rmd", "report_template.docx", "TASA_logo_full_color.png"), overwrite = TRUE)
             out <- render(
-                "report_catchproj_1per.Rmd",
-                params = list(user_name = input$name, datafile = df_upload_catchproj_1per()),
+                "report_fisher_1per.Rmd",
+                params = list(user_name = input$name, datafile = df_upload_fisher_1per()),
                 envir = new.env(parent = globalenv())
             )
             file.rename(out, file)
         }
     )
-    output$report_lamp_1per <- downloadHandler(
-        filename = "report_lamp_1per.docx",
+    output$report_lampgen_1per <- downloadHandler(
+        filename = "report_lampgen_1per.docx",
         content = function(file) {
-            src <- normalizePath(c("reports/report_lamp_1per.Rmd", "reports/report_template.docx", "www/images/TASA_logo_full_color.png"))
+            src <- normalizePath(c("reports/report_lampgen_1per.Rmd", "reports/report_template.docx", "www/images/TASA_logo_full_color.png"))
             owd <- setwd(tempdir())
             on.exit(setwd(owd))
-            file.copy(src, c("report_lamp_1per.Rmd", "report_template.docx", "TASA_logo_full_color.png"), overwrite = TRUE)
+            file.copy(src, c("report_lampgen_1per.Rmd", "report_template.docx", "TASA_logo_full_color.png"), overwrite = TRUE)
             out <- render(
-                "report_lamp_1per.Rmd",
-                params = list(user_name = input$name, datafile = df_upload_lamp_1per()),
+                "report_lampgen_1per.Rmd",
+                params = list(user_name = input$name, datafile = df_upload_lampgen_1per()),
                 envir = new.env(parent = globalenv())
             )
             file.rename(out, file)
         }
     )
-    output$report_spag_1per <- downloadHandler(
-        filename = "report_spag_1per.docx",
+    output$report_spagvis_1per <- downloadHandler(
+        filename = "report_spagvis_1per.docx",
         content = function(file) {
-            src <- normalizePath(c("reports/report_spag_1per.Rmd", "reports/report_template.docx", "www/images/TASA_logo_full_color.png"))
+            src <- normalizePath(c("reports/report_spagvis_1per.Rmd", "reports/report_template.docx", "www/images/TASA_logo_full_color.png"))
             owd <- setwd(tempdir())
             on.exit(setwd(owd))
-            file.copy(src, c("report_spag_1per.Rmd", "report_template.docx", "TASA_logo_full_color.png"), overwrite = TRUE)
+            file.copy(src, c("report_spagvis_1per.Rmd", "report_template.docx", "TASA_logo_full_color.png"), overwrite = TRUE)
             out <- render(
-                "report_spag_1per.Rmd",
-                params = list(user_name = input$name, datafile = df_upload_spag_1per()),
+                "report_spagvis_1per.Rmd",
+                params = list(user_name = input$name, datafile = df_upload_spagvis_1per()),
                 envir = new.env(parent = globalenv())
             )
             file.rename(out, file)
--- ui.r
+++ ui.r
@@ -87,7 +87,7 @@ ui <- navbarPage(
                                     "Choose Time Period",
                                     div(
                                         class = "input-list-content",
-                                        prettyRadioButtons("period_catch",
+                                        prettyRadioButtons("period_fisheries",
                                             label = NULL,
                                             choices = c("One Season", "One Year", "Multiple Seasons", "Multiple Years"), selected = "One Season", inline = TRUE
                                         )
@@ -100,7 +100,7 @@ ui <- navbarPage(
                                     div(
                                         class = "input-list-content",
                                         fileInput(
-                                            "upload_catch_1per",
+                                            "upload_fisheries_1per",
                                             label = NULL,
                                             multiple = FALSE,
                                             accept = c(
@@ -117,7 +117,7 @@ ui <- navbarPage(
                                     "Validate Data",
                                     div(
                                         class = "input-list-content",
-                                        actionButton("validate_catch_1per", "Perform Validation")
+                                        actionButton("validate_fisheries_1per", "Perform Validation")
                                     )
                                 )
                             )
@@ -141,7 +141,7 @@ ui <- navbarPage(
                                     "Generate Report",
                                     div(
                                         class = "input-list-content",
-                                        downloadButton("report_catch_1per", "Generate")
+                                        downloadButton("report_fisheries_1per", "Generate")
                                     )
                                 )
                             )
@@ -173,7 +173,7 @@ ui <- navbarPage(
                                     "Choose Time Period",
                                     div(
                                         class = "input-list-content",
-                                        prettyRadioButtons("period_catchproj",
+                                        prettyRadioButtons("period_fisher",
                                             label = NULL,
                                             choices = c("One Season", "One Year", "Multiple Seasons", "Multiple Years"), selected = "One Season", inline = TRUE
                                         )
@@ -186,7 +186,7 @@ ui <- navbarPage(
                                     div(
                                         class = "input-list-content",
                                         fileInput(
-                                            "upload_catchproj_1per",
+                                            "upload_fisher_1per",
                                             label = NULL,
                                             multiple = FALSE,
                                             accept = c(
@@ -203,7 +203,7 @@ ui <- navbarPage(
                                     "Validate Data",
                                     div(
                                         class = "input-list-content",
-                                        actionButton("validate_catchproj_1per", "Perform Validation")
+                                        actionButton("validate_fisher_1per", "Perform Validation")
                                     )
                                 )
                             )
@@ -227,7 +227,7 @@ ui <- navbarPage(
                                     "Generate Report",
                                     div(
                                         class = "input-list-content",
-                                        downloadButton("report_catchproj_1per", "Generate")
+                                        downloadButton("report_fisher_1per", "Generate")
                                     )
                                 )
                             )
@@ -259,7 +259,7 @@ ui <- navbarPage(
                                     "Choose Datatype",
                                     div(
                                         class = "input-list-content",
-                                        prettyRadioButtons("datatype_lamp", label = NULL, choices = c("Conch", "General LAMP"), selected = "Conch", inline = TRUE)
+                                        prettyRadioButtons("datatype_lampgen", label = NULL, choices = c("Conch", "General LAMP"), selected = "Conch", inline = TRUE)
                                     )
                                 ),
                                 hr(),
@@ -268,7 +268,7 @@ ui <- navbarPage(
                                     "Choose Time Period",
                                     div(
                                         class = "input-list-content",
-                                        prettyRadioButtons("period_lamp",
+                                        prettyRadioButtons("period_lampgen",
                                             label = NULL,
                                             choices = c("One Period", "Multiple Periods"), selected = "One Period", inline = TRUE
                                         )
@@ -281,7 +281,7 @@ ui <- navbarPage(
                                     div(
                                         class = "input-list-content",
                                         fileInput(
-                                            "upload_lamp_1per",
+                                            "upload_lampgen_1per",
                                             label = NULL,
                                             multiple = FALSE,
                                             accept = c(
@@ -298,7 +298,7 @@ ui <- navbarPage(
                                     "Validate Data",
                                     div(
                                         class = "input-list-content",
-                                        actionButton("validate_lamp_1per", "Perform Validation")
+                                        actionButton("validate_lampgen_1per", "Perform Validation")
                                     )
                                 )
                             )
@@ -322,7 +322,7 @@ ui <- navbarPage(
                                     "Generate Report",
                                     div(
                                         class = "input-list-content",
-                                        downloadButton("report_lamp_1per", "Generate")
+                                        downloadButton("report_lampgen_1per", "Generate")
                                     )
                                 )
                             )
@@ -354,7 +354,7 @@ ui <- navbarPage(
                                     "Choose Datatype",
                                     div(
                                         class = "input-list-content",
-                                        prettyRadioButtons("datatype_spag", label = NULL, choices = c("Visual Census", "Laser Data"), selected = "Visual Census", inline = TRUE)
+                                        prettyRadioButtons("datatype_spagvis", label = NULL, choices = c("Visual Census", "Laser Data"), selected = "Visual Census", inline = TRUE)
                                     )
                                 ),
                                 hr(),
@@ -363,7 +363,7 @@ ui <- navbarPage(
                                     "Choose Time Period",
                                     div(
                                         class = "input-list-content",
-                                        prettyRadioButtons("period_spag", label = NULL, choices = c("One Year", "Multiple Years"), selected = "One Year", inline = TRUE)
+                                        prettyRadioButtons("period_spagvis", label = NULL, choices = c("One Year", "Multiple Years"), selected = "One Year", inline = TRUE)
                                     )
                                 ),
                                 hr(),
@@ -373,7 +373,7 @@ ui <- navbarPage(
                                     div(
                                         class = "input-list-content",
                                         fileInput(
-                                            "upload_spag_1per",
+                                            "upload_spagvis_1per",
                                             label = NULL,
                                             multiple = FALSE,
                                             accept = c(
@@ -390,7 +390,7 @@ ui <- navbarPage(
                                     "Validate Data",
                                     div(
                                         class = "input-list-content",
-                                        actionButton("validate_spag_1per", "Perform Validation")
+                                        actionButton("validate_spagvis_1per", "Perform Validation")
                                     )
                                 )
                             )
@@ -414,7 +414,7 @@ ui <- navbarPage(
                                     "Generate Report",
                                     div(
                                         class = "input-list-content",
-                                        downloadButton("report_spag_1per", "Generate")
+                                        downloadButton("report_spagvis_1per", "Generate")
                                     )
                                 )
                             )
