291724faef832d1193a401d01159060cf9acc2d2
Konkaseeti <konkasuseter@gmail.com>
2025-05-15 16:47:54 -0600

    Add historic basemap shapefiles Add historic basemap functions

--- map.r
+++ map.r
@@ -27,6 +27,16 @@ areas_map <- st_read("CA.shp") %>%
         "Special Management Area" = "Special Management"
     ))
 
+areas_map_historic <- st_read("Zoning_January_Final_Proposal_2_Dissolve_General.shp") %>%
+    st_transform(4326) %>%
+    filter(CLASS %in% c("Spawning Aggregation Site Reserve", "Conservation", "Preservation", "Special Management Area")) %>%
+    mutate(name = recode(CLASS,
+        "Spawning Aggregation Site Reserve" = "Spawning Aggregation Site",
+        "Conservation" = "Conservation",
+        "Preservation" = "Preservation",
+        "Special Management Area" = "Special Management"
+    ))
+
 # Create Function to Make Dynamic Map ---------------------------
 generate_map <- function(df, lat, long, site_id) {
     df$Long <- -abs(df[[long]])
@@ -70,6 +80,48 @@ generate_map <- function(df, lat, long, site_id) {
         )
 }
 
+generate_map_historic <- function(df, lat, long, site_id) {
+    df$Long <- -abs(df[[long]])
+    df$Lat <- df[[lat]]
+    df_sf <- st_as_sf(df, coords = c("Long", "Lat"), crs = 4326)
+    ggplot() +
+        geom_sf(data = ecosystems_map, aes(fill = ECOSYSTEM), linewidth = 0, color = "transparent") +
+        scale_fill_manual(values = c(
+            "Seagrass" = "#bbe9ffff",
+            "Caribbean Sea" = "#ebfaffff",
+            "Mangrove/Littoral Forest" = "#c9ffcbff",
+            "Coral Reef" = "#bdb7e3ff"
+        ), name = "Ecosystem") +
+        new_scale_fill() +
+        geom_sf(data = areas_map_historic, aes(fill = name), alpha = 0.85, linewidth = 0, color = "transparent") +
+        scale_fill_manual(values = c(
+            "Spawning Aggregation Site" = "#ef9a95ff",
+            "Conservation" = "#f6f89dff",
+            "Preservation" = "#f7bf9aff",
+            "Special Management" = "#f5c8d5ff"
+        ), name = "Zone") +
+        geom_sf(data = df_sf, size = 2, color = "#302f2f", fill = "white", shape = 21) +
+        geom_text_repel(
+            data = df, aes(x = Long, y = Lat, label = !!sym(site_id)),
+            size = 2.5, color = "black", fontface = "bold", box.padding = 0.5,
+            point.padding = 0.5, force = 5, max.overlaps = 50,
+            segment.color = "#555555", segment.size = 0.5
+        ) +
+        coord_sf(xlim = c(-88.05, -87.66), ylim = c(17.117024, 17.664305)) +
+        annotation_scale(location = "br", width_hint = 0.2, style = "ticks") +
+        annotation_north_arrow(
+            location = "tl", which_north = "true",
+            style = north_arrow_fancy_orienteering
+        ) +
+        theme_minimal() +
+        theme(
+            legend.text = element_text(size = 8),
+            legend.title = element_text(size = 9),
+            legend.key.size = unit(0.5, "cm"),
+            legend.spacing.y = unit(0.2, "cm")
+        )
+}
+
 # Create Function to Make Dynamic Map Without Labels ---------------------------
 generate_map_nolabels <- function(df, lat, long, site_id) {
     df$Long <- -abs(df[[long]])
@@ -85,6 +137,42 @@ generate_map_nolabels <- function(df, lat, long, site_id) {
         ), name = "Ecosystem") +
         new_scale_fill() +
         geom_sf(data = areas_map, aes(fill = name), alpha = 0.85, linewidth = 0, color = "transparent") +
+        scale_fill_manual(values = c(
+            "Spawning Aggregation Site" = "#ef9a95ff",
+            "Conservation" = "#f6f89dff",
+            "Preservation" = "#f7bf9aff",
+            "Special Management" = "#f5c8d5ff"
+        ), name = "Zone") +
+        geom_sf(data = df_sf, size = 2, color = "#302f2f", fill = "white", shape = 21) +
+        coord_sf(xlim = c(-88.05, -87.66), ylim = c(17.117024, 17.664305)) +
+        annotation_scale(location = "br", width_hint = 0.2, style = "ticks") +
+        annotation_north_arrow(
+            location = "tl", which_north = "true",
+            style = north_arrow_fancy_orienteering
+        ) +
+        theme_minimal() +
+        theme(
+            legend.text = element_text(size = 8),
+            legend.title = element_text(size = 9),
+            legend.key.size = unit(0.5, "cm"),
+            legend.spacing.y = unit(0.2, "cm")
+        )
+}
+
+generate_map_nolabels_historic <- function(df, lat, long, site_id) {
+    df$Long <- -abs(df[[long]])
+    df$Lat <- df[[lat]]
+    df_sf <- st_as_sf(df, coords = c("Long", "Lat"), crs = 4326)
+    ggplot() +
+        geom_sf(data = ecosystems_map, aes(fill = ECOSYSTEM), linewidth = 0, color = "transparent") +
+        scale_fill_manual(values = c(
+            "Seagrass" = "#bbe9ffff",
+            "Caribbean Sea" = "#ebfaffff",
+            "Mangrove/Littoral Forest" = "#c9ffcbff",
+            "Coral Reef" = "#bdb7e3ff"
+        ), name = "Ecosystem") +
+        new_scale_fill() +
+        geom_sf(data = areas_map_historic, aes(fill = name), alpha = 0.85, linewidth = 0, color = "transparent") +
         scale_fill_manual(values = c(
             "Spawning Aggregation Site" = "#ef9a95ff",
             "Conservation" = "#f6f89dff",
