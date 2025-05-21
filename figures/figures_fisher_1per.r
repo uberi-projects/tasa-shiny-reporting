eee48ef8ceda5c5f6c5e85795872a7a874103ccc
sharp-lilac <161522137+sharp-lilac@users.noreply.github.com>
2025-05-21 10:56:11 -0600

    Support plotlist for demo figure download
    For fisher catch 1per test figure download

--- figures/figures_fisher_1per.r
+++ figures/figures_fisher_1per.r
@@ -7,11 +7,12 @@ library(ggplot2)
 output$figures_fisher_1per <- downloadHandler(
     filename = function() {
         datatype <- isolate(input$datatype_fisher_1per)
-        paste0("figure_fisher_1per_", datatype, ".zip")
+        paste0("figure_fisher_1per_", tolower(datatype), ".zip")
     },
     content = function(zipfile) {
         datatype <- isolate(input$datatype_fisher_1per)
-        p <- switch(datatype,
+        plot_list <- list()
+        p1 <- switch(datatype,
             "Conch" = ggplot(iris, aes(Sepal.Width, Sepal.Length, color = Species)) +
                 geom_point() +
                 theme_classic(),
@@ -23,14 +24,25 @@ output$figures_fisher_1per <- downloadHandler(
                 theme_void(),
             stop("Unknown datatype: ", datatype)
         )
-        img_name <- paste0("figure_fisher_1per_", datatype, ".png")
-        tmp_png <- file.path(tempdir(), img_name)
-        ggsave(tmp_png, plot = p, device = "png", width = 6, height = 4)
-        zip::zipr(
-            zipfile,
-            files = tmp_png,
-            root = tempdir()
+        plot_list[[1]] <- list(
+            plot = p1,
+            name = paste0("figure_fisher_1per_", tolower(datatype), "_1.png")
         )
+        if (datatype == "Conch") {
+            p2 <- ggplot(iris, aes(x = Species, fill = as.factor(Petal.Width))) +
+                geom_bar() +
+                theme_classic()
+            plot_list[[2]] <- list(
+                plot = p2,
+                name = paste0("figure_fisher_1per_", tolower(datatype), "_2.png")
+            )
+        }
+        tmp_files <- lapply(plot_list, function(item) {
+            tmp <- file.path(tempdir(), item$name)
+            ggsave(tmp, plot = item$plot, device = "png", width = 6, height = 4)
+            tmp
+        })
+        zip::zipr(zipfile, files = unlist(tmp_files), root = tempdir())
     },
     contentType = "application/zip"
 )
