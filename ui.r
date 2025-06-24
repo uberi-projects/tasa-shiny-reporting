c3f62090f028a9615988694a3405a295c5a5fe10
sharp-lilac <161522137+sharp-lilac@users.noreply.github.com>
2025-06-24 08:39:30 -0600

    Remove temporary cover from fisher multiper

--- ui.r
+++ ui.r
@@ -103,7 +103,7 @@ ui <- navbarPage(
                     })"
                         )
                     ),
-                    div( # Temporary cover for LAMP and SPAG reports.
+                    div( # Temporary cover
                         style = "position: relative; left: -15px; top:195px;",
                         div(
                             class = "temporary-cover",
@@ -296,7 +296,7 @@ ui <- navbarPage(
                     })"
                         )
                     ),
-                    div( # Temporary cover for LAMP and SPAG reports.
+                    div( # Temporary cover
                         style = "position: relative; left: -15px; top:195px;",
                         div(
                             class = "temporary-cover",
@@ -712,45 +712,6 @@ ui <- navbarPage(
                     });
                     "
                         )
-                    ),
-                    div( # Temporary cover for LAMP and SPAG reports.
-                        style = "position: relative; left: -15px; top:195px;",
-                        div(
-                            class = "temporary-cover",
-                            style = "
-                                position: fixed;
-                                height: 100vh;
-                                width: 100vw;
-                                background-color: rgba(200, 200, 200, 0.5);
-                                z-index: 3;
-                                margin-left: 0px;
-                            ",
-                            div(
-                                style = "
-                                    position: relative;
-                                    width: 25%;
-                                    height: 25%;
-                                    background-color: rgba(255,255,255,0.6);
-                                    margin: auto;
-                                    top: 160px;
-                                    border-radius: 10px;
-                                    ",
-                                h1(
-                                    "Coming Soon!",
-                                    style = "
-                                    position: absolute;
-                                    text-align: center;
-                                    top: 50%;
-                                    left: 50%;
-                                    margin: 0;
-                                    transform: translate(-50%, -50%);
-                                    font-size: 2em;
-                                    font-weight: 800;
-                                    color: black;
-                                    "
-                                )
-                            )
-                        ),
                     ),
                     div(
                         class = "content-container-parent",
@@ -1335,7 +1296,7 @@ ui <- navbarPage(
                     });"
                         )
                     ),
-                    div( # Temporary cover for LAMP and SPAG reports.
+                    div( # Temporary cover
                         style = "position: relative; left: -15px; top:195px;",
                         div(
                             class = "temporary-cover",
@@ -1513,7 +1474,7 @@ ui <- navbarPage(
                     });"
                         )
                     ),
-                    div( # Temporary cover for LAMP and SPAG reports.
+                    div( # Temporary cover
                         style = "position: relative; left: -15px; top:195px;",
                         div(
                             class = "temporary-cover",
